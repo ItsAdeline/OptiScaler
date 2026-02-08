c9ff24795ca323657ab111d3184036f3809dc06d
TheRazerMD <derazer1950@gmail.com>
2026-02-08 18:06:20 +0100

    Clang fix

--- OptiScaler/dllmain.cpp
+++ OptiScaler/dllmain.cpp
@@ -46,8 +46,6 @@
 static std::vector<HMODULE> _asiHandles;
 static bool _passThruMode = false;
 
-
-
 typedef const char*(CDECL* PFN_wine_get_version)(void);
 typedef void (*PFN_InitializeASI)(void);
 typedef bool (*PFN_PatchResult)(void);
